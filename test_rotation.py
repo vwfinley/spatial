e081f84c2db8f66f40f3acb9c59838497608bc78
vwfinley <35928742+vwfinley@users.noreply.github.com>
2018-01-29 14:23:09 -0500

    Made methods private.

--- test_rotation.py
+++ test_rotation.py
@@ -9,7 +9,7 @@ from rotation import Rotation
 class RotationTests(unittest.TestCase):
     """Tests for 'rotation.py'."""
 
-    def check_matrix(self, theta, m):
+    def __check_matrix(self, theta, m):
         """ Checks the elements of a 2D 3x3 affine rotation matrix """
         s = math.sin(theta)
         c = math.cos(theta)
@@ -26,23 +26,23 @@ class RotationTests(unittest.TestCase):
         self.assertEqual(m[2][1], 0.0)
         self.assertEqual(m[2][2], 1.0)
 
-    def check(self, theta):
+    def __check(self, theta):
         """tests if rotation matrix is correctly initialized some angle theta"""
         rot = Rotation(theta)
-        print(rot.m)
-        self.check_matrix(theta, rot.m)
+#        print(rot.m)
+        self.__check_matrix(theta, rot.m)
 
     def test_matrix_0_pi(self):
-        """tests if rotation matrix is correctly initialized for 45 degs"""
-        self.check(0)
+        """tests if rotation matrix is correctly initialized for 0 degs"""
+        self.__check(0)
 
     def test_matrix_pi_4(self):
         """tests if rotation matrix is correctly initialized for 45 degs"""
-        self.check(math.pi / 4)
+        self.__check(math.pi / 4)
 
     def test_matrix_pi_2(self):
         """tests if rotation matrix is correctly initialized for 90 degs"""
-        self.check(math.pi / 2)
+        self.__check(math.pi / 2)
 
 if __name__ == '__main__':
     unittest.main()
