b3a813887b26e612d3690e0037a8b815a5774b18
vwfinley <35928742+vwfinley@users.noreply.github.com>
2018-01-31 15:17:41 -0500

    Added point unit tests.  Fixed size of point vector.

--- point.py
+++ point.py
@@ -1,45 +1,87 @@
+"""
+    Module defines a Point class to be used in 2D spatial transformations.
+"""
 
 from numpy import ones
-
 from conversion import cart2pol
 
 class Point:
+    """
+        2D Point class to be used in 2D spatial transformations.  To use, get the vector
+        returned by the affine property and matrix multiply it with a transformation matrix.
+    """
+
     def __init__(self, name, x, y):
+        """
+            Constructor for Point class.
+
+            name = name of the point, example: "Point5"
+            x = x position of point
+            y = y position of point
+        """
         self._name = name
-        self.p = ones((1, 3))
-        self.p[0][0] = x
-        self.p[0][1] = y
+        self.m = ones((3, 1))
+        self.m[0] = x
+        self.m[1] = y
 
     @property
     def name(self):
+        """
+            Gets name of point.
+        """
         return self._name
 
     @property
     def x(self):
-        return self.p[0][0]
+        """
+            Gets x position of point.
+        """
+        return self.m[0]
 
     @property
     def y(self):
-        return self.p[0][1]
+        """
+            Gets y position of point.
+        """
+        return self.m[1]
 
     @property        
     def r(self):
+        """
+            Gets the distance (radius) of point from origin.
+        """
         pol = cart2pol(self.x, self.y)
-        return pol[0][0]
+        return pol[0]
 
     @property
     def theta(self):
+        """
+            Gets the point's angle, theta, with respect to the x-axis.
+        """       
         pol = cart2pol(self.x, self.y)
-        return pol[0][1]
+        return pol[1]
     
     @property
     def polar(self):
+        """
+            Gets the point's location in polar coords. The array [r, theta] is returned.
+        """
         return cart2pol(self.x, self.y)
     
     @property
     def cart(self):
+        """
+            Gets the point's location in cartesian coords. The array [x, y] is returned.
+        """       
         return [self.x, self.y]
 
-    @property
+    @property   
     def affine(self):
-        return self.p
+        """
+            Gets the point's 2D affine vector representation.
+            The vector [ x,
+                         y,
+                         z, ] 
+            is returned.
+        """              
+        return self.m
