12723ee5b8c2bcfc54c2c4a1a991911288c94930
vwfinley <35928742+vwfinley@users.noreply.github.com>
2018-01-31 15:36:02 -0500

    Added docstrings

--- conversion.py
+++ conversion.py
@@ -1,11 +1,32 @@
+"""
+    Module contains basic conversions.  For example
+    between polar and cartesian coords.
+"""
+
 from math import sin, cos, hypot, atan2
 
-def cart2pol(x, y):
+def cart2pol(x, y):   
+    """
+        Converts x, y to polar coords.
+
+        x = position along x-axis.
+        y = position along y-axis.
+
+        returns [r, theta]
+    """
     r = hypot(x, y)
     theta = atan2(y, x)
     return [r, theta]
 
 def pol2cart(r, theta):
+    """
+        Converts r, theta to cartesian coords.
+
+        r = distance of a point from the origin.
+        theta = angle of the point with respect to the x-axis.
+
+        returns [x, y]
+    """
     x = r * cos(theta)
     y = r * sin(theta)
     return [x, y]
--- point.py
+++ point.py
@@ -1,6 +1,4 @@
-"""
-    Module defines a Point class to be used in 2D spatial transformations.
-"""
+"""Module defines a Point class to be used in 2D spatial transformations."""
 
 from numpy import ones
 from conversion import cart2pol
@@ -26,62 +24,46 @@ class Point:
 
     @property
     def name(self):
-        """
-            Gets name of point.
-        """
+        """Gets name of point."""
         return self._name
 
     @property
     def x(self):
-        """
-            Gets x position of point.
-        """
+        """Gets x position of point."""
         return self.m[0]
 
     @property
     def y(self):
-        """
-            Gets y position of point.
-        """
+        """Gets y position of point."""
         return self.m[1]
 
-    @property        
+    @property
     def r(self):
-        """
-            Gets the distance (radius) of point from origin.
-        """
+        """Gets the distance (radius) of point from origin."""
         pol = cart2pol(self.x, self.y)
         return pol[0]
 
     @property
     def theta(self):
-        """
-            Gets the point's angle, theta, with respect to the x-axis.
-        """       
+        """Gets the point's angle, theta, with respect to the x-axis."""
         pol = cart2pol(self.x, self.y)
         return pol[1]
-    
+
     @property
     def polar(self):
-        """
-            Gets the point's location in polar coords. The array [r, theta] is returned.
-        """
+        """Gets the point's location in polar coords. The array [r, theta] is returned."""
         return cart2pol(self.x, self.y)
-    
+
     @property
     def cart(self):
-        """
-            Gets the point's location in cartesian coords. The array [x, y] is returned.
-        """       
+        """Gets the point's location in cartesian coords. The array [x, y] is returned."""
         return [self.x, self.y]
 
-    @property   
+    @property
     def affine(self):
-        """
-            Gets the point's 2D affine vector representation.
-            The vector [ x,
-                         y,
-                         z, ] 
-            is returned.
-        """              
+        """Gets the point's 2D affine vector representation.
+            The vector [x
+                        y
+                        z]
+            is returned."""
         return self.m
