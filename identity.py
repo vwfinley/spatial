7ac6ca6dd9d91dfab78036f1e8cde24fcc9ab590
vwfinley <35928742+vwfinley@users.noreply.github.com>
2018-02-01 12:22:41 -0500

    Added scripts to call unittests.

--- identity.py
+++ identity.py
@@ -4,15 +4,15 @@ Module defines an 2D affine identity matrix.
 import numpy
 
 class Identity:
-    """ 
+    """
         Trivial class that defines a 2D affine (3x3) identity matrix as follows:
         [
             [1.0, 0.0, 0.0],
             [0.0, 1.0, 0.0],
             [0.0, 0.0, 1.0]
-        ]   
+        ]
     """
 
     def __init__(self):
-        """Constructs Identity matrix"""       
+        """Constructs Identity matrix"""
         self.m = numpy.identity(3)
